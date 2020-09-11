7c1a8821b5daba725e9a79fad78e9a742e90b51b
phlc <pedrocarvalho@outlook.com>
2020-09-11 12:48:52 -0300

    Lista_Apagados.java Adequacao de terminologias

--- Lista_Apagados.java
+++ Lista_Apagados.java
@@ -17,26 +17,26 @@ import java.io.*;
 /*
 Lista Encadeada para ser usada como índice direto para registros apagados
 CHAVE: Short - Tamanho do registro
-VALOR: Long - Posição do registro no arquivo
+DADO:  Long  - Posição do registro no arquivo
 
 Formato da lista:
 Byte 0-1: Short - Número de nodes da lista apagados
-Byte 2-9: Long - Posição do primeiro node da lista
-Byte 10 - Nodes
+Byte 2-9: Long  - Posição do primeiro node da lista
+Byte 10.: Nodes 
 
 Formato do node:
-Short - tamanho do registro no arquivo originário 
-Long - Posição do registro no arquivo originário
-Long - Posição do próximo node
+Short - chave:     tamanho do registro no arquivo originário 
+Long  - dado:      Posição do registro no arquivo originário
+Long  - prox_node: Posição do próximo node
 */
 
 
 public class Lista_Apagados {
    
 //atributos da classe
-   final private static int TAMANHO_NODE = 18; //Short+Long+Long
-   final private static short POS_APAGADOS = 0; //Posicao do numero de nodes pagados
-   final private static long INICIO = 2; //Posicao do endereco do primeiro node    
+   final private static int TAMANHO_NODE = 18;    //Short+Long+Long
+   final private static short POS_APAGADOS = 0;   //Posicao do numero de nodes pagados
+   final private static long INICIO = 2;          //Posicao do endereco do primeiro node    
    final private static String CONTROLE_A = "a_"; //início nome do arquivo para controle
    final private static String CONTROLE_B = "b_"; //início nome do arquivo para controle
 
@@ -74,11 +74,11 @@ public class Lista_Apagados {
 //metodos
    /*
    read - Le da lista a posição de um registro apagado no principal com tamanho suficiente
-   @param short tamanho necessário
-   @return long posição ou -1 se inexistente
+   @param short chave
+   @return long dado ou -1 se inexistente
    */
-   public long read(short tamanho){
-      long resp = -1; //resposta
+   public long read(short chave){
+      long dado = -1; //resposta
       long node;      //posição primeiro node
 
       //ler a posicao do primeiro node da lista
@@ -87,48 +87,72 @@ public class Lista_Apagados {
       
       //se a lista não está vazia, buscar recursivamente      
       if(node != -1)
-         resp = read(tamanho, node);
+         dado = read(chave, node);
       
-      return resp;
+      return dado;
    }
    /*
-   sobrecarga de read
-   @param short tamanho, long node
+   overload read
+   @param short chave, long node
    */
-   private long read(short tamanho, long node){
-      long resp = -1; //resposta
+   private long read(short chave, long node){
+      long dado = -1; //resposta
       short tam_lido; //tamanho do registro referenciado pelo node
-      long posicao;   //posição do registro referenciado pelo node
+      long dado_lido; //posição do registro referenciado pelo node
       long prox_node; //posição do próximo node
       
       //ler dados do arquivo
       arquivo.seek(node);
       tam_lido = arquivo.readShort();
-      posicao = arquivo.readLong();
+      dado_lido = arquivo.readLong();
       prox_node = arquivo.readLong();
+      
       //verificar tamanho do registro referenciado
-      if(tamanho < tam_lido)
-         resp = posicao;
+      if(chave <= tam_lido)
+         dado = dado_lido;
       else if (prox_node != -1)
-         resp = read(tamanho, prox_node);
+         dado = read(tamanho, prox_node);
 
-      return resp;
+      return dado;
    }
 
    /*
    insert - insere um novo node na lista de forma ordenada
    @param short tamanho long posicao
-   @return boolean confirmacao
    */
-   public boolean insert(short tamanho, long posicao){
-      boolean resp = false; //resposta
+   public void insert(short tamanho, long dado){
       long node;            //posição primeiro node
-      
+      long endereco;        //endereco a ser escrito      
+
       //ler a posição do primeiro node da lista
       arquivo.seek(INICIO);
-   
-   
-   ]
+      node = readLong();
+
+      //se a lista nao está vazia, inserir na posicao certar recursivamente
+      if(node != -1){
+         endereco = insert(tamanho, posicao, node);
+      } 
+      //lista vazia, criar primeiro node
+      else{
+         //armazenar endereco do novo node e ir para a posicao
+         endereco = arq.length(); //posicao do novo node
+         arq.seek(endereco);
+
+         //escrever dados
+         arquivo.writeShort(tamanho);
+         arquivo.writeLong(posicao);
+         arquivo.writeLong(-1);
+      }
+
+      //verificar se INICIO mudou
+      if(node != endereco){
+         arquivo.seek(INICIO);
+         arquivo.writeLong(endereco);
+      }
+   }
+   /*
+   overload insert
+   @param short tamanho, long dado, long node
 
 
 }
